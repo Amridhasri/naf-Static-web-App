72e88320b619f13c3d305a5d66134c0f7cbcca8c
Amridhasri <99492442+Amridhasri@users.noreply.github.com>
2023-06-27 11:50:24 +0530

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,30 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { RxComponent } from './rx/rx.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { UserComponent } from './user/user.component';
 
-import { AppComponent } from "./app.component";
+import { MovieListComponent } from './movie-list/movie-list.component';
+import { MovieCardComponent } from './movie-card/movie-card.component';
 
+// import { CommonModule } from '@angular/common';
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [
+    AppComponent,
+    RxComponent,
+    UserComponent,
+    
+    MovieListComponent,
+    MovieCardComponent,
+  //  CommonModule,
+  ],
+  imports: [
+    BrowserModule,ReactiveFormsModule,HttpClientModule,FormsModule,
+  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
